4585ab92f064767bc662f0ebd8a52f364c2f5787
Trozer Wincitore <trozer@live.com>
2018-04-07 22:20:38 +0200

    Presenters added

---
+++ app/src/main/java/com/example/mobsoft/cryptobet/ui/details/CryptoDetailsPresenter.java
@@ -0,0 +1,20 @@
+package com.example.mobsoft.cryptobet.ui.details;
+
+import com.example.mobsoft.cryptobet.ui.Presenter;
+
+public class CryptoDetailsPresenter extends Presenter<CryptoDetailsScreen> {
+    @Override
+    public void attachScreen(CryptoDetailsScreen screen) {
+        super.attachScreen(screen);
+    }
+
+    @Override
+    public void detachScreen() {
+        super.detachScreen();
+    }
+
+    public void saveBid(){};
+
+    public void clearBid(){};
+
+}
---
+++ app/src/main/java/com/example/mobsoft/cryptobet/ui/main/MainPresenter.java
@@ -0,0 +1,21 @@
+package com.example.mobsoft.cryptobet.ui.main;
+
+import com.example.mobsoft.cryptobet.ui.Presenter;
+
+public class MainPresenter extends Presenter<MainScreen> {
+    @Override
+    public void attachScreen(MainScreen screen) {
+        super.attachScreen(screen);
+    }
+
+    @Override
+    public void detachScreen() {
+        super.detachScreen();
+    }
+
+    public void showCryptonCurrencyDetails(){
+
+    };
+
+    public void setScore(){};
+}
